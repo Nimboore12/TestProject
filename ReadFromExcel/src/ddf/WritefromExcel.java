efc0697fb3d9b1d319be8182082666eb5e47de2d
Dell <Dell@DESKTOP-0VJ6AUA>
2021-07-31 14:02:29 +0530

    First Commit

---
+++ ReadFromExcel/src/ddf/WritefromExcel.java
@@ -0,0 +1,14 @@
+package ddf;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class WritefromExcel {
+
+	public static void main(String[] args) throws IOException {
+		
+		FileInputStream File =new FileInputStream(new File(System.getProperty("user.dir")+"\\input.xlsx"));
+	}
+
+}
